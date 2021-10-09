935f05ebcfed9a2a3d7ec137f8fc1cd355347e8f
codesyariah122 <pujiermanto@gmail.com>
2021-10-09 13:29:31 +0700

    update router

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const dotenv = require('dotenv').config()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('products.json')
 const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 7000;
